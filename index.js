abd5264d088446c119e06267deec21f468cf2aa0
Aditya Gaurav <adityagauravstartup@gmail.com>
2025-05-28 23:44:12 +0530

    Use process.env.PORT for Render deployment

--- index.js
+++ index.js
@@ -47,4 +47,5 @@ io.on("connection", socket => {
   });
 });
 
-server.listen(3000, () => console.log(`HTTP Server running on PORT 3000`));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`HTTP Server is running on PORT ${PORT}`));
